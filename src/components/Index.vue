ffc04f51ac983f0473ffad795294ca515b6c2093
Lhourquin <lhourquin@gmail.com>
2019-05-02 00:58:57 +0200

    remove HeLLoWorld.vue and create Index.vue formy test

---
+++ src/components/Index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>{{post.voiture}} {{post.totem}}</h1>
+    
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'Index',
+    props : ['post']
+}
+</script>
+
+<style>
+
+</style>
